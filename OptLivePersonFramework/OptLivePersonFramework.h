5f386c421986387463f57ad555c132ee1caa753a
Muthu Rama <muthu.rama@sandstone.com.au>
2019-01-31 11:21:03 +1100

    Initial Commit

---
+++ OptLivePersonFramework/OptLivePersonFramework.h
@@ -0,0 +1,19 @@
+//
+//  OptLivePersonFramework.h
+//  OptLivePersonFramework
+//
+//  Created by Muthu Rama on 31/1/19.
+//  Copyright © 2019 Muthu Rama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OptLivePersonFramework.
+FOUNDATION_EXPORT double OptLivePersonFrameworkVersionNumber;
+
+//! Project version string for OptLivePersonFramework.
+FOUNDATION_EXPORT const unsigned char OptLivePersonFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OptLivePersonFramework/PublicHeader.h>
+
+
